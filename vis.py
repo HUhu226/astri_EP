5bab2edaae96eb9fd42036792aef0ffb51c9325b
Chesley <ziqi.ch@outlook.com>
2025-05-19 13:58:12 +0800

    modify vis.py

--- vis.py
+++ vis.py
@@ -10,3 +10,13 @@ with open(file_path, "rb") as f:
 # 打印内容类型和部分数据
 print(type(data))
 print(data)
+
+
+
+# 访问数据中的特定字段
+# 这里假设数据是一个字典，包含了你需要的字段
+# 你可以根据实际数据结构进行修改
+# 例如，假设数据包含一个名为 'placement' 的字段
+# if 'placement' in data:
+#     placement = data['placement']
+#     print("Placement data:", placement)
